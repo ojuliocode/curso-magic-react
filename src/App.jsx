d833f055edd59778eea6464cca3b9c83d9919875
ojuliocode <juliojuriollif@gmail.com>
2024-07-31 08:24:12 -0300

    finalizacao

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,24 @@ import Projetos from './componentes/Projetos/Projetos'
 import "./App.css"
 import { useState } from 'react'
 
-function App(){
+function App() {
   const [projetos, setProjetos] = useState([
-    {linkDoGithub: 'https://github.com', caminhoDaImagem: 'src/assets/facebook.png'},
-    {linkDoGithub: 'https://github.com', caminhoDaImagem: 'src/assets/tesla.png'},
-    {linkDoGithub: 'https://github.com', caminhoDaImagem: 'src/assets/vite.png'},
+    { linkDoGithub: 'https://github.com', caminhoDaImagem: 'src/assets/facebook.png' },
+    { linkDoGithub: 'https://github.com', caminhoDaImagem: 'src/assets/tesla.png' },
+    { linkDoGithub: 'https://github.com', caminhoDaImagem: 'src/assets/vite.png' },
   ])
   return (
     <div className="conteudo-principal">
       <Header />
       <Conteudo />
-      <Projetos projetos={projetos}/>
+      <Projetos projetos={projetos} />
       <footer className="footer">
-        <p>
-          Feito por @ojuliocode
-        </p>
+        
+        <div>
+          <h4> Contato: </h4>
+          <p> ojuliocode@gmail.com </p>
+          <p> 31911112222</p>
+        </div>
       </footer>
     </div>
   )
